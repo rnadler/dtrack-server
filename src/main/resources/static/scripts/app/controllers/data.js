72c09cbee73b78bdfe170acd74c71e25e5d4c618
rnadler <robert.nadler@gmail.com>
2016-02-13 16:30:40 -0800

    T-18: Add type search to data page

--- src/main/resources/static/scripts/app/controllers/data.js
+++ src/main/resources/static/scripts/app/controllers/data.js
@@ -1,7 +1,22 @@
 'use strict';
 
-angular.module('dtrackApp').controller('DataCtrl', function($scope, $http) {
-  $http.get('/entries/').success(function(data) {
-    $scope.data = data['_embedded']['entries'];
-  })
+angular.module('dtrackApp').controller('DataCtrl', function ($scope, $http) {
+    $scope.searchTerm = '';
+
+    $scope.ok = function () {
+        $scope.getData($scope.searchTerm);
+    };
+    $scope.checkKeyUp = function (event) {
+        return event.keyCode == 13 && $scope.ok();
+    };
+    $scope.getData = function (search) {
+        var searchParam = '';
+        if (search !== undefined && search.length > 0) {
+            searchParam = 'search/findByType?type=' + search;
+        }
+        $http.get('/entries/' + searchParam).success(function (data) {
+            $scope.data = data['_embedded']['entries'];
+        })
+    };
+    $scope.getData();
 });
