0ac64a16bd76934ba0f7dbe71c5593d998c550c2
rnadler <robert.nadler@gmail.com>
2016-04-02 16:04:33 -0700

    T-24: Remove auto-generated database rest interface

--- src/main/java/com/rdn/model/EntryRepository.java
+++ src/main/java/com/rdn/model/EntryRepository.java
@@ -3,11 +3,9 @@ package com.rdn.model;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
-@RepositoryRestResource(collectionResourceRel = "entries", path = "entries")
 public interface EntryRepository extends MongoRepository<Entry, String> {
     List<Entry> findByType(@Param("type") String type);
     List<Entry> findByUser(@Param("user") String user);
