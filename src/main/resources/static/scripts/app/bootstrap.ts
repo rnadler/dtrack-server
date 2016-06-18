9d19dc66d6fa6aac5bd19552a7026a1c7a344d6b
rnadler <robert.nadler@gmail.com>
2016-06-18 12:09:49 -0700

    T-26 Minor angular2 changes -- still a WIP

--- src/main/resources/static/scripts/app/bootstrap.ts
+++ src/main/resources/static/scripts/app/bootstrap.ts
@@ -1,8 +1,15 @@
 import {upgradeAdapter} from './upgrade_adapter';
 import {UpdateComponent} from './components/updateComponent/update_component';
 /* . . . */
-// declare var angular:any;
-// angular.module('dtrackApp', []).directive('updateComponent', upgradeAdapter.downgradeNg2Component(UpdateComponent));
 upgradeAdapter.upgradeNg1Provider('User');
 upgradeAdapter.upgradeNg1Provider('$stomp');
+// upgradeAdapter.upgradeNg1Component('appFooter');
+// upgradeAdapter.upgradeNg1Component('logAlert');
+// upgradeAdapter.upgradeNg1Component('navBar');
+// upgradeAdapter.upgradeNg1Component('notificationAlert');
+// upgradeAdapter.upgradeNg1Component('register');
+
+// declare var angular:any;
+// angular.module('dtrackApp', []).directive('updateComponent', upgradeAdapter.downgradeNg2Component(UpdateComponent));
+
 upgradeAdapter.bootstrap(document.body, ['dtrackApp']);
